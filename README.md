453749f3e2a638d44e529378ba10448c3a7ff426
kris <k.kromolicki@gmail.com>
2024-02-25 23:26:58 +0100

    globals

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-https://blog.elektroweb.pl/bme280-czujnik-wilgotnosci-cisnienia-i-temperatury-podpiecie-do-arduino/
+## DoIT ESP32 DEVKIT V1 
+## ESP-WROOM-32
+### FCC ID: 2AC7Z-ESPWROOM32
+
+![Image](doc/DOIT-ESP32-DevKit-v1.svg)
+
+### Links: 
+- [ESP32 Wikipedia](https://en.wikipedia.org/wiki/ESP32)
+- [BME280 - Czujnik wilgotności, ciśnienia i temperatury - Podpięcie do Arduino](https://blog.elektroweb.pl/bme280-czujnik-wilgotnosci-cisnienia-i-temperatury-podpiecie-do-arduino/)
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,106 +1,106 @@
-#include <iostream>
-#include "globals.h"
+  #include <iostream>
+  #include "globals.h"
 
-#include <WiFi.h>
-#include <Wire.h>
-#include <Adafruit_BME280.h>
-#include <Adafruit_Sensor.h>
+  #include <WiFi.h>
+  #include <Wire.h>
+  #include <Adafruit_BME280.h>
+  #include <Adafruit_Sensor.h>
 
-// 77m npm = 1007.60 hpa
-// (11,3 hPa/100m)
-#define SEALEVELPRESSURE_HPA (1007.60) 
+  // 77m npm = 1007.60 hpa
+  // (11,3 hPa/100m)
+  #define SEALEVELPRESSURE_HPA (1007.60) 
 
-Adafruit_BME280 bme; 
+  Adafruit_BME280 bme; 
 
-WiFiServer server(80);
+  WiFiServer server(80);
 
-String header;
+  String header;
 
-unsigned long currentTime = millis();
-unsigned long previousTime = 0; 
-const long timeoutTime = 2000;
+  unsigned long currentTime = millis();
+  unsigned long previousTime = 0; 
+  const long timeoutTime = 2000;
 
-void setup() {
-  Serial.begin(115200);
-  bool status;
+  void setup() {
+    Serial.begin(115200);
+    bool status;
 
-  if (!bme.begin(0x76)) {
-    Serial.println("Could not find a valid BME280 sensor, check wiring!");
-    while (1);
+    if (!bme.begin(0x76)) {
+      Serial.println("Could not find a valid BME280 sensor, check wiring!");
+      while (1);
+    }
+
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+    }
+    Serial.println("");
+    Serial.println("WiFi connected.");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+    server.begin();
   }
 
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.println("WiFi connected.");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-  server.begin();
-}
+  void loop(){
+    WiFiClient client = server.available();
 
-void loop(){
-  WiFiClient client = server.available();
-
-  if (client) {                          
-    currentTime = millis();
-    previousTime = currentTime;
-    Serial.println("New Client.");       
-    String currentLine = "";                
-    while (client.connected() && currentTime - previousTime <= timeoutTime) { 
+    if (client) {                          
       currentTime = millis();
-      if (client.available()) {             
-        char c = client.read();            
-        Serial.write(c);                   
-        header += c;
-        if (c == '\n') {
-          if (currentLine.length() == 0) {
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-type:text/html");
-            client.println("Connection: close");
-            client.println();
-            client.println("<!DOCTYPE html><html><meta charset=\"utf-8\"><title>ESP32 - BME280</title>");
-            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
-            client.println("<link rel=\"icon\" href=\"data:,\">");
-            client.println("<style>body { text-align: center; font-family: \"Trebuchet MS\", Arial;}");
-            client.println("table { border-collapse: collapse; width:35%; margin-left:auto; margin-right:auto; }");
-            client.println("th { padding: 12px; background-color: #0043af; color: white; }");
-            client.println("tr { border: 1px solid #ddd; padding: 12px; }");
-            client.println("tr:hover { background-color: #bcbcbc; }");
-            client.println("td { border: none; padding: 12px; }");
-            client.println(".sensor {font-weight: bold; padding: 1px; }");            
-            client.println("</style></head><body><h1>ESP32 - BME280</h1>");
-            client.println("<table><tr><th>Pomiar</th><th>Wartość</th></tr>");
-            client.println("<tr><td>Temperatura</td><td><span class=\"sensor\">");
-            client.println(bme.readTemperature());
-            client.println(" *C</span></td></tr>");  
-            client.println("<tr><td>Ciśnienie</td><td><span class=\"sensor\">");
-            client.println(bme.readPressure() / 100.0F);
-            client.println(" hPa</span></td></tr>");
-            client.println("<tr><td>Przybliżona wysokość</td><td><span class=\"sensor\">");
-            client.println(bme.readAltitude(SEALEVELPRESSURE_HPA));
-            client.println(" m</span></td></tr>"); 
-            client.println("<tr><td>Wilgotność</td><td><span class=\"sensor\">");
-            client.println(bme.readHumidity());
-            client.println(" %</span></td></tr>"); 
-            client.println("</body></html>");            
-            client.println();
-            break;
-          } else {
-            currentLine = "";
+      previousTime = currentTime;
+      Serial.println("New Client.");       
+      String currentLine = "";                
+      while (client.connected() && currentTime - previousTime <= timeoutTime) { 
+        currentTime = millis();
+        if (client.available()) {             
+          char c = client.read();            
+          Serial.write(c);                   
+          header += c;
+          if (c == '\n') {
+            if (currentLine.length() == 0) {
+              client.println("HTTP/1.1 200 OK");
+              client.println("Content-type:text/html");
+              client.println("Connection: close");
+              client.println();
+              client.println("<!DOCTYPE html><html><meta charset=\"utf-8\"><title>ESP32 - BME280</title>");
+              client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+              client.println("<link rel=\"icon\" href=\"data:,\">");
+              client.println("<style>body { text-align: center; font-family: \"Trebuchet MS\", Arial;}");
+              client.println("table { border-collapse: collapse; width:35%; margin-left:auto; margin-right:auto; }");
+              client.println("th { padding: 12px; background-color: #0043af; color: white; }");
+              client.println("tr { border: 1px solid #ddd; padding: 12px; }");
+              client.println("tr:hover { background-color: #bcbcbc; }");
+              client.println("td { border: none; padding: 12px; }");
+              client.println(".sensor {font-weight: bold; padding: 1px; }");            
+              client.println("</style></head><body><h1>ESP32 - BME280</h1>");
+              client.println("<table><tr><th>Pomiar</th><th>Wartość</th></tr>");
+              client.println("<tr><td>Temperatura</td><td><span class=\"sensor\">");
+              client.println(bme.readTemperature());
+              client.println(" *C</span></td></tr>");  
+              client.println("<tr><td>Ciśnienie</td><td><span class=\"sensor\">");
+              client.println(bme.readPressure() / 100.0F);
+              client.println(" hPa</span></td></tr>");
+              client.println("<tr><td>Przybliżona wysokość</td><td><span class=\"sensor\">");
+              client.println(bme.readAltitude(SEALEVELPRESSURE_HPA));
+              client.println(" m</span></td></tr>"); 
+              client.println("<tr><td>Wilgotność</td><td><span class=\"sensor\">");
+              client.println(bme.readHumidity());
+              client.println(" %</span></td></tr>"); 
+              client.println("</body></html>");            
+              client.println();
+              break;
+            } else {
+              currentLine = "";
+            }
+          } else if (c != '\r') { 
+            currentLine += c;
           }
-        } else if (c != '\r') { 
-          currentLine += c;
         }
       }
+      header = "";
+      client.stop();
+      Serial.println("Client disconnected.");
+      Serial.println("");
     }
-    header = "";
-    client.stop();
-    Serial.println("Client disconnected.");
-    Serial.println("");
   }
-}
